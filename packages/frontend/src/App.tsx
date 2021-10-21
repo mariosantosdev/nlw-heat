f13201791c0e92304437aef983655d85554e7435
Mario Santos <mariodev7@gmail.com>
2021-10-21 05:45:23 -0300

    feat: sending message to database

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import styles from './styles/App.module.scss';
+import { useUserContext } from './contexts/UserContext';
 
 import LoginBox from './components/LoginBox';
 import MessageList from './components/MessageList';
+import SendMessageForm from './components/SendMessageForm';
 
 function App() {
+    const { user } = useUserContext();
     return (
         <main className={styles.contentWrapper}>
             <MessageList />
-            <LoginBox />
+            {user ? <SendMessageForm /> : <LoginBox />}
         </main>
     )
 }
--- packages/frontend/src/components/LoginBox/index.tsx
+++ packages/frontend/src/components/LoginBox/index.tsx
@@ -1,12 +1,17 @@
 import { VscGithubInverted } from 'react-icons/vsc';
+import { useUserContext } from '../../contexts/UserContext';
 
 import styles from './styles.module.scss';
 
 export default function LoginBox() {
+    const { signInUrl, user } = useUserContext();
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}><VscGithubInverted scale='24' />Entrar com GitHub</a>
+            <a href={signInUrl} className={styles.signInWithGithub}>
+                <VscGithubInverted scale='24' />
+                Entrar com GitHub
+            </a>
         </div>
     )
 }
