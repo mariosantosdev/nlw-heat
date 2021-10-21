5775cca5970ae6d60da2b92e4bfa228d3e7bea8f
Mario Santos <mariodev7@gmail.com>
2021-10-21 05:19:51 -0300

    feat: created user context

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { UserProvider } from './contexts/UserContext'
 
 import './styles/global.scss'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <UserProvider>
+            <App />
+        </UserProvider>
     </React.StrictMode>,
     document.getElementById('root')
 )
